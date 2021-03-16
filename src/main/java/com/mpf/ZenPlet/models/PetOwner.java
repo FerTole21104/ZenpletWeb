3d4efa39815892c4107fd0c6a37427a715624147
Dormamud03 <paulsanunga@gmail.com>
2021-03-16 15:50:14 -0500

    Entidades PetOwner vaccines

--- src/main/java/com/mpf/ZenPlet/models/PetOwner.java
+++ src/main/java/com/mpf/ZenPlet/models/PetOwner.java
@@ -1,5 +1,69 @@
 package com.mpf.ZenPlet.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "Pet_Owner")
 public class PetOwner {
-    
+    // Atributos
+    @Id
+    @Column(name = "owner_pet_id", nullable = false)
+    private long ownerPetId;
+    @Column(name = "pet_id")
+    private long petId;
+    @Column(name = "owner_id")
+    private long ownerId;
+
+    @JsonBackReference
+    @JoinColumn(name = "owner_id")
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Owner owner;
+    @JsonBackReference
+    @JoinColumn(name = "pet_id")
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Pet pet;
+
+    // Constructor
+    public PetOwner() {
+    }
+
+    public PetOwner(long ownerPetId, long petId, long ownerId) {
+        this.ownerPetId = ownerPetId;
+        this.petId = petId;
+        this.ownerId = ownerId;
+    }
+
+    // Getters And Setters
+    public long getOwnerPetId() {
+        return ownerPetId;
+    }
+
+    public void setOwnerPetId(long ownerPetId) {
+        this.ownerPetId = ownerPetId;
+    }
+
+    public long getPetId() {
+        return petId;
+    }
+
+    public void setPetId(long petId) {
+        this.petId = petId;
+    }
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(long ownerId) {
+        this.ownerId = ownerId;
+    }
+
 }
--- src/main/java/com/mpf/ZenPlet/models/Vaccines.java
+++ src/main/java/com/mpf/ZenPlet/models/Vaccines.java
@@ -1,4 +1,66 @@
 package com.mpf.ZenPlet.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "vaccines")
 public class Vaccines {
-    
+    // Atributos
+    @Id
+    @Column(name = "vaccines_id", nullable = false)
+    private long vaccinesID;
+    @Column(name = "vaccines_name")
+    private String vaccinesName;
+    @Column(name = "vaccines_description")
+    private String vaccinesDescription;
+
+    @JsonBackReference
+    @JoinColumn(name = "vaccines_id")
+    @ManyToMany(fetch = FetchType.LAZY)
+    private PetVaccines petVaccines;
+
+    // Constructores
+    public Vaccines() {
+    }
+
+    public Vaccines(long vaccinesID, String vaccinesName, String vaccinesDescription) {
+        this.vaccinesID = vaccinesID;
+        this.vaccinesName = vaccinesName;
+        this.vaccinesDescription = vaccinesDescription;
+    }
+
+    // Getters and Setters
+
+    public String getVaccinesName() {
+        return vaccinesName;
+    }
+
+    public void setVaccinesName(String vaccinesName) {
+        this.vaccinesName = vaccinesName;
+    }
+
+    public String getVaccinesDescription() {
+        return vaccinesDescription;
+    }
+
+    public void setVaccinesDescription(String vaccinesDescription) {
+        this.vaccinesDescription = vaccinesDescription;
+    }
+
+    public long getVaccinesID() {
+        return vaccinesID;
+    }
+
+    public void setVaccinesID(long vaccinesID) {
+        this.vaccinesID = vaccinesID;
+    }
+
 }
