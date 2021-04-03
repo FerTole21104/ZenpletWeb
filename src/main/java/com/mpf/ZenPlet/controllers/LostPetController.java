b5d1f4f67dee80e15401efdd591b3a372d3a2529
Mikadifo <michaelpadillaheredia@hotmail.com>
2021-04-02 21:22:59 -0500

    added find pet on list of lost pets using pet id

--- src/main/java/com/mpf/ZenPlet/controllers/LostPetController.java
+++ src/main/java/com/mpf/ZenPlet/controllers/LostPetController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import com.mpf.ZenPlet.models.LostPet;
+import com.mpf.ZenPlet.models.Pet;
 import com.mpf.ZenPlet.repositories.LostPetRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/*@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)*/
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class LostPetController {
@@ -28,6 +30,11 @@ public class LostPetController {
     @GetMapping("/lost-pet")
     public List<LostPet> getLostPets() {
         return lostPetRepository.findAll();
+    }
+
+    @RequestMapping(value = "/lost-pet/pet/{petId}", method = RequestMethod.GET)
+    public Pet getLostPetByPetId(@PathVariable("petId") long petId) {
+        return this.lostPetRepository.findPetByPetId(petId);
     }
 
     /*
--- src/main/java/com/mpf/ZenPlet/repositories/LostPetRepository.java
+++ src/main/java/com/mpf/ZenPlet/repositories/LostPetRepository.java
@@ -1,17 +1,13 @@
 package com.mpf.ZenPlet.repositories;
 
-import java.util.List;
-
 import com.mpf.ZenPlet.models.LostPet;
+import com.mpf.ZenPlet.models.Pet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-/*import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;*/
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LostPetRepository extends JpaRepository<LostPet, Long> {
-    /*
-     * @Query(value =
-     * "SELECT lp from Lost_Pet lp where lp.entidadName like %:name%") public
-     * List<LostPet> findByName(@Param("name")String name);
-     */
+    @Query(value = "SELECT lp.pet from LostPet lp where lp.id.petId = :petId")
+    public Pet findPetByPetId(@Param("petId") long petId);
 }
