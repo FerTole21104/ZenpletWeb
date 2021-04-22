bb17abe75462d34ebd8ba352c789612eec91e1c6
Dormamud03 <paulsanunga@gmail.com>
2021-04-22 15:12:29 -0500

    test los metodos estan listos bueno solo el primero

--- src/test/java/com/mpf/ZenPlet/ZenPletApplicationTests.java
+++ src/test/java/com/mpf/ZenPlet/ZenPletApplicationTests.java
@@ -1,15 +1,41 @@
 package com.mpf.ZenPlet;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @SpringBootTest
-class ZenPletApplicationTests{
+class ZenPletApplicationTests {
+
+        @Autowired
+        MockMvc mock;
 
         @Test
-        void contextLoads(){
-            
-        }  
+        @Order(0)
+        void contextLoads() throws Exception {
+                mock.perform(MockMvcRequestBuilders.post("/api/save-owner").contentType(MediaType.APPLICATION_JSON))
+                                .andDo(MockMvcResultHandlers.print());
+        }
 
+        @Test
+        @Order(1)
+        void testCreacion() throws Exception {
+                mock.perform(MockMvcRequestBuilders.post("/crear").contentType(MediaType.APPLICATION_JSON)
+                                // .content("{\"userId\":2,\"username\":\"lucho\",\"contrasena\":\"123\",\"nombreCompleto\":\"Luis
+                                // Perez\"}"))
+                                .content("{\"username\":\"lucho\",\"contrasena\":\"123\",\"nombreCompleto\":\"Luis Perez\"}"))
+                                .andDo(MockMvcResultHandlers.print());
+        }
+
+        @Test
+        @Order(2)
+        void testListar() throws Exception {
+                mock.perform(MockMvcRequestBuilders.get("/listar-usuarios")).andDo(MockMvcResultHandlers.print());
+        }
 
 }
