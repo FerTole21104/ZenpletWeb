d9730d22a4fd0c175a7fb84ea6c8b19f46b9e771
Fernanda Toledo <maria.toledo.est@tecazuay.edu.ec>
2021-04-21 20:42:35 -0500

    test

---
+++ src/test/java/com/mpf/ZenPlet/ZenPletApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mpf.ZenPlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenPletApplicationTests{
+
+        @Test
+        void contextLoads(){
+            
+        }  
+
+
+}
